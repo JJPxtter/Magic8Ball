ee8a9ab34498117987e701d03fa4a792175fb6c6
Jeandre Putter <jeandre.putter@gmail.com>
2022-08-09 14:09:21 +0200

    test

--- MagicBall.cs
+++ MagicBall.cs
@@ -1,8 +1,8 @@
 namespace Magic8Ball
 {
-    public partial class Form1 : Form
+    public partial class MagicBall : Form
     {
-        public Form1()
+        public MagicBall()
         {
             InitializeComponent();
         }
