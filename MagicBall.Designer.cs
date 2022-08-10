3d18c79496c98ab14c76fc92637a66ed75c63726
Jeandre Putter <jeandre.putter@gmail.com>
2022-08-10 19:14:40 +0200

    Validation etc

--- MagicBall.Designer.cs
+++ MagicBall.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Magic8Ball
 {
-    partial class MagicBall
+    partial class MagicEightBall
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,20 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MagicEightBall));
+            this.submitButton = new System.Windows.Forms.Button();
+            this.questionBox = new System.Windows.Forms.TextBox();
+            this.answerLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // MagicBall
+            // submitButton
+            // 
+            this.submitButton.BackColor = System.Drawing.Color.Transparent;
+            this.submitButton.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.submitButton.ForeColor = System.Drawing.Color.Black;
+            this.submitButton.Location = new System.Drawing.Point(311, 600);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(252, 45);
+            this.submitButton.TabIndex = 0;
+            this.submitButton.Text = "Tell Me!";
+            this.submitButton.UseVisualStyleBackColor = false;
+            this.submitButton.Click += new System.EventHandler(this.submitButton_Click);
+            // 
+            // questionBox
+            // 
+            this.questionBox.Location = new System.Drawing.Point(311, 523);
+            this.questionBox.Multiline = true;
+            this.questionBox.Name = "questionBox";
+            this.questionBox.Size = new System.Drawing.Size(252, 64);
+            this.questionBox.TabIndex = 1;
+            this.questionBox.Text = "Ask any question here...";
+            // 
+            // answerLabel
+            // 
+            this.answerLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.answerLabel.BackColor = System.Drawing.Color.Transparent;
+            this.answerLabel.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.answerLabel.ForeColor = System.Drawing.Color.Black;
+            this.answerLabel.Location = new System.Drawing.Point(311, 266);
+            this.answerLabel.Name = "answerLabel";
+            this.answerLabel.Size = new System.Drawing.Size(252, 117);
+            this.answerLabel.TabIndex = 2;
+            this.answerLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // MagicEightBall
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "MagicBall";
+            this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(895, 668);
+            this.Controls.Add(this.answerLabel);
+            this.Controls.Add(this.questionBox);
+            this.Controls.Add(this.submitButton);
+            this.DoubleBuffered = true;
+            this.Name = "MagicEightBall";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Magic 8 Ball";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Button submitButton;
+        private TextBox questionBox;
+        private Label answerLabel;
     }
 }
--- MagicBall.cs
+++ MagicBall.cs
@@ -1,10 +1,60 @@
 namespace Magic8Ball
 {
-    public partial class MagicBall : Form
+    public partial class MagicEightBall : Form
     {
-        public MagicBall()
+
+        Random random = new Random();
+        List<String> answers = new List<String>();
+
+        public MagicEightBall()
         {
             InitializeComponent();
+            answers.Add("It is certain.");
+            answers.Add("It is decidedly so.");
+            answers.Add("Without a doubt.");
+            answers.Add("Yes definitely.");
+            answers.Add("You may rely on it.");
+            answers.Add("As I see it, yes.");
+            answers.Add("Most likely.");
+            answers.Add("Outlook good.");
+            answers.Add("Yes.");
+            answers.Add("Signs point to yes.");
+            answers.Add("Reply hazy, try again");
+            answers.Add("Ask again later.");
+            answers.Add("Better not tell you now.");
+            answers.Add("Cannot predict now.");
+            answers.Add("Concentrate and ask again.");
+            answers.Add("Don't count on it.");
+            answers.Add("My reply is no.");
+            answers.Add("My sources say no.");
+            answers.Add("Outlook not so good.");
+            answers.Add("Very doubtful.");
+
+
+        }
+
+        private void submitButton_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(questionBox.Text))
+            {
+                answerLabel.Text = "";
+                MessageBox.Show("Please ask a Question");
+                return;
+            }
+            if (questionBox.Text.Contains("Ask any question here..."))
+            {
+                answerLabel.Text = "";
+                MessageBox.Show("Please ask a Question");
+                return;
+            }
+            if (questionBox.Text.Contains("?") == false)
+            {
+                answerLabel.Text = "";
+                MessageBox.Show("Please ask a Question");
+                return;
+            }
+            int index = random.Next(answers.Count);
+            answerLabel.Text = answers[index];
         }
     }
 }
